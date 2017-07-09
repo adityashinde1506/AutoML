7ba011496d31d05a38aac419d1c2099a087c1cd3
Aditya Shinde <shinde.aditya386@gmail.com>
2017-07-09 19:40:31 -0400

    Added column shifting.

--- mllib/dataset/source.py
+++ mllib/dataset/source.py
@@ -9,12 +9,13 @@ logger=logging.getLogger(__name__)
 
 class Datasource(object):
 
-    def __init__(self,files,headers,delimiter=",",chunk=None,remove_cols=[],remove_ind=[],transforms=[],test_split=0.3,target_col="y"):
+    def __init__(self,files,headers,delimiter=",",chunk=None,remove_cols=[],remove_ind=[],transforms=[],rolls=[],test_split=0.3,target_col="y"):
         self.dataset=Dataset(files,delimiter=delimiter,chunk=chunk)
         self.remove_cols=remove_cols
         self.remove_ind=remove_ind
         self.headers=headers
         self.transforms=transforms
+        self.rolls=rolls
         if chunk==None:
             self.IN_MEM=True
         else:
@@ -22,7 +23,7 @@ class Datasource(object):
         self.target=target_col
         self.split=test_split
         logger.info("Datasource initialized.")
-        logger.debug("Datasource: MEM:{} DISCARD:{} TARGET:{}".format(self.IN_MEM,",".join(self.remove_cols),self.target))
+        logger.debug("Datasource: MEM:{} DISCARD:{} TARGET:{} FEATURES:{}".format(self.IN_MEM,",".join(self.remove_cols),self.target,", ".join(self.headers)))
 
     def __remove_cols(self,dataframe):
         for col in self.remove_cols:
@@ -42,6 +43,16 @@ class Datasource(object):
             frame=transform(frame)
         return frame
 
+    def __apply_roll(self,frame):
+        max_roll=0
+        for col,steps in self.rolls:
+            logger.debug("Rolling {} by {}.".format(col,steps))
+            if steps>max_roll:
+                max_roll=steps
+            frame[col]=frame[col].shift(steps)
+        logger.debug("Removing {} rows to compensate for roll.".format(max_roll))
+        return frame[max_roll:]
+
     def prepare_sets(self,X,y):
         X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=self.split,random_state=42)
         self.sets={"train":(X_train,y_train),"test":(X_test,y_test)}
@@ -54,6 +65,7 @@ class Datasource(object):
             data.columns=self.headers
             data=self.__apply_transforms(data)
             data=self.__remove_cols(data)
+            data=self.__apply_roll(data)
             X,y=self.__get_data_matrices(data)
             self.prepare_sets(X,y)
             return self.sets
@@ -68,6 +80,7 @@ class Datasource(object):
             data.columns=self.headers
             data=self.__apply_transforms(data)
             data=self.__remove_cols(data)
+            data=self.__apply_roll(data)
             return data
         else:
             # TODO: return batch
