1d7a8b87f76c55f365d190124d944b5365835fe2
Aditya Shinde <shinde.aditya386@gmail.com>
2017-12-20 16:17:55 -0500

    Added base data loader functionality.

--- mltools/dataset/dataloader.py
+++ mltools/dataset/dataloader.py
@@ -3,10 +3,15 @@ import logging
 logger=logging.getLogger(__name__)
 
 from pathlib import Path
+from itertools import groupby
 
 def path_visitor(path,names,found):
+    '''
+        Recursively traverses directory and looks for given files.
+    '''
     for name in names:
         if name==path.parts[-1]:
+            logger.debug(f"Found file {str(path)}")
             found.append(str(path))
     if not path.is_dir():
         return
@@ -22,4 +27,26 @@ def get_data_files(entry_dir,filenames):
     entry_dir=Path(entry_dir)
     found=[]
     path_visitor(entry_dir,filenames,found)
+    logger.info(f"Found {len(found)} data files under {str(entry_dir)}.")
     return found
+
+def group_filenames(groupnames):
+    '''
+        Splits given list of file names into a dict of given groups. Should
+        be used in case data is put in files named according to labels.
+    '''
+    assert type(groupnames)==list, "groupnames should be a list."
+#    assert type(filenames)==list, "filenames should be a list."
+
+    def namer(filename):
+        for name in groupnames:
+            if name.lower() in filename.lower():
+                return name
+        return "UNKNOWN"
+
+    def grouper(filenames):
+        #groups=groupby(map(namer,filenames),key=lambda x:x[0])
+        groups=groupby(filenames,key=namer)
+        return groups
+
+    return grouper
--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,6 +1,8 @@
 import unittest
 import sys
+import logging
 
+logging.basicConfig(level=logging.INFO)
 sys.path.append("/home/adityas/Projects/AutoML")
 
 from mltools.dataset.dataloader import *
@@ -12,4 +14,11 @@ class TestDirLoader(unittest.TestCase):
 
     def test_file_getter(self):
         files=get_data_files(self.path,["ztomatrix.txt"])
-        self.assertTrue(len(files)>0)
+        self.assertIsNotNone(files)
+
+    def test_grouper(self):
+        files=get_data_files(self.path,["ztomatrix.txt"])
+        grouper=group_filenames(["Aggregation","Collection","Broadcast","Consensus","DGD"])
+        data_groups=grouper(files)
+        self.assertEqual(len(list(data_groups)),6)
+        
